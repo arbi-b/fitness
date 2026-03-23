527a1767172575496623ac9eef78e3a725f49c0d
xugv25 <arbi.backa@gmail.com>
2026-03-23 21:06:52 +0100

    exercise gifs second try

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -16,11 +16,20 @@ export default function ExercisesPage() {
       <h1>Exercise Library</h1>
 
       {exercises.map((ex: any) => (
-        <div key={ex.id} style={{ marginBottom: "30px" }}>
+        <div
+            key={ex.id}
+            style={{
+            marginBottom: "30px",
+            padding: "20px",
+            border: "1px solid #ddd",
+            borderRadius: "12px",
+            maxWidth: "350px",
+            }}
+        >
           <h3>{ex.name}</h3>
 
           {/* THIS SHOWS THE GIF */}
-          <img src={ex.gifUrl} alt={ex.name} width={300} />
+          <img src={ex.gifUrl} alt={ex.name}  width={300}  loading="lazy" style={{ borderRadius: "10px", marginTop: "10px" }}/>
 
           <p><strong>Target:</strong> {ex.target}</p>
           <p><strong>Body Part:</strong> {ex.bodyPart}</p>
